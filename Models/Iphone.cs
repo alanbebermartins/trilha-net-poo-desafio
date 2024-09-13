0b18aee085e6b5efa385127b0407377a03bfc223
alanbebermartins <abebermartins@gmail.com>
2024-09-13 15:07:25 -0300

    Classe Iphone implementada com a herança smartphone

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,22 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
+    // IMPLEMENTADO
     public class Iphone
     {
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        // IMPLEMENTADO
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Aplicativo {nomeApp} selecionado para instalação.");
+            Console.WriteLine("Metodo de pagamento selecionado: Débito.");
+            Console.WriteLine("Confirmação de token para validação do pagamento.");
+            Console.WriteLine("Verificação de segurança Apple...");
+            Console.WriteLine("Instalando...");
+            Console.WriteLine($"O aplicativo {nomeApp} do celular Nokia foi instalado com sucesso!");
+        }
     }
 }
